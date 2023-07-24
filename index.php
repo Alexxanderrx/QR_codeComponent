55dda2fdcae953cf75bef10ebfc2795d203fa893
Alexxanderrx <alexmorales196@gmail.com>
2023-07-24 18:39:55 -0500

    terminado usando tailwindCSS

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+    <link href="./output.css" rel="stylesheet">
+    <title>Frontend Mentor | QR code component</title>
+</head>
+
+<body class="flex justify-center items-center bg-[#d6e2f0] w-screen h-screen">
+    <div class="flex flex-col justify-between items-center bg-white rounded-xl w-64 h-[410px] p-4">
+        <div class="rounded-xl overflow-hidden w-56 h-56">
+            <img src="./images/image-qr-code.png" alt="image-qr-code.png">
+        </div>
+        <div class="flex flex-col justify-around items-center h-36 p-2">
+            <p class="text-[#1f3251] font-bold text-center" style="font-family: 'Outfit', sans-serif;"
+                class="font-bold">
+                Improve your front-end skills by building projects
+            </p>
+
+            <p class="text-[#9a9da6] font-normal text-xs text-center" style="font-family: 'Outfit', sans-serif;">
+                Scan theQR code tovisit Frontend Mentor and take your coding skills to the next level
+            </p>
+        </div>
+    </div>
+    <!-- <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+    </div> -->
+</body>
+
+</html>
